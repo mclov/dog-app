70f4de24bfce68cccdbffad5fec1e5bf6b4513ee
Michael Clovis <michaelclovis@protonmail.com>
2019-12-10 12:25:58 -0800

    add dogs info & route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import whiskey from './images/puppy1.jpeg';
+import hazel from './images/puppy2.jpeg';
+import tubby from './images/puppy3.jpeg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        {/* <Navbar dogs={this.props.dogs} /> */}
+        <div className='container'>
+          <Route exact path='/dogs' render={() => <h1>Dog List</h1>} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
