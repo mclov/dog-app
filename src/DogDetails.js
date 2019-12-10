a8f582181b534166e181c847ff0f8a52ec815af3
Michael Clovis <michaelclovis@protonmail.com>
2019-12-10 15:02:34 -0800

    add page links to doglist

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -6,25 +6,23 @@ export class DogDetail extends Component {
   render() {
     let { dog } = this.props
     return (
-      <div className='container'>
-        <div className='DogDetails row justify-content-center mt-5'>
-          <div className='col-11 col-lg-5'>
-            <div className='DogDetails-card card'>
-              <img src={dog.src} alt={dog.name} />
-              <div className='card-body'>
-                <h2 className='card-title'>{dog.name}</h2>
-                <h4 className='card-subtitle text-muted'>
-                  {dog.age} year{dog.age === 1 ? '' : 's'} old
+      <div className='DogDetails row justify-content-center mt-5'>
+        <div className='col-11 col-lg-5'>
+          <div className='DogDetails-card card'>
+            <img src={dog.src} alt={dog.name} />
+            <div className='card-body'>
+              <h2 className='card-title'>{dog.name}</h2>
+              <h4 className='card-subtitle text-muted'>
+                {dog.age} year{dog.age === 1 ? '' : 's'} old
                 </h4>
-              </div>
-              <ul className='list-group list-group-flush'>
-                {dog.facts.map((fact, i) =>
-                  <li className='list-group-item' key={i}>{fact}</li>
-                )}
-              </ul>
-              <div className='card-body'>
-                <Link to='/dogs' className='btn btn-info'>Go Back</Link>
-              </div>
+            </div>
+            <ul className='list-group list-group-flush'>
+              {dog.facts.map((fact, i) =>
+                <li className='list-group-item' key={i}>{fact}</li>
+              )}
+            </ul>
+            <div className='card-body'>
+              <Link to='/dogs' className='btn btn-info'>Go Back</Link>
             </div>
           </div>
         </div>
