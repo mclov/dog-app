a871ec03a1880cfc88ea182393e909a7aa702617
Michael Clovis <michaelclovis@protonmail.com>
2019-12-10 15:43:27 -0800

    add styling

--- src/DogList.js
+++ src/DogList.js
@@ -6,13 +6,13 @@ export class DogList extends Component {
   render() {
     return (
       <div className='DogList'>
-        <h1 className='display-1 text-center'>Dog List</h1>
+        <h1 className='display-1 text-center my-4'>Dog List</h1>
         <div className='row'>
           {this.props.dogs.map(d => (
             <div className='Dog col-lg-4 text-center' key={d.name} >
               <Link to={`/dogs/${d.name}`}>
                 <img src={d.src} alt={d.name} />
-                <h3>{d.name}</h3>
+                <h3 className='underline'>{d.name}</h3>
               </Link>
             </div>
           ))}
